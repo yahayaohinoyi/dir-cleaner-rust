d8e9a3cdea466f96b95d74f9ff0c9a7220cbd33c
yahayaohinoyi <yahaya.suleiman2204@gmail.com>
2024-09-15 15:17:12 +0100

    test: made test clearer

--- src/main.rs
+++ src/main.rs
@@ -47,16 +47,24 @@ mod tests {
     fn test_directory_cleaner_should_delete_files_greater_than_the_specified_min_size() -> Result<()>
     {
         let temp_dir = tempdir()?;
-        let file_path = temp_dir.path().join("test.txt");
-        let file = File::create(&file_path)?;
-        file.set_len(3000)?; // 3000 bytes
+        let file_path_1 = temp_dir.path().join("test1.txt");
+        let file_path_2 = temp_dir.path().join("test2.txt");
+        let file_1 = File::create(&file_path_1)?;
+        let file_2 = File::create(&file_path_2)?;
+        file_1.set_len(4000)?; // 3000 bytes
+        file_2.set_len(500)?; // 500 bytes
 
         let dir_str = temp_dir.path().to_str().unwrap().to_string();
         features::cleaner_file_size::directory_cleaner_based_on_file_size(&dir_str, 2000)?;
 
         assert!(
-            !file_path.exists(),
+            !file_path_1.exists(),
             "File should be deleted as it's more than the minimum size"
+        );
+
+        assert!(
+            file_path_2.exists(),
+            "File should not be deleted as it's less than the minimum size"
         );
 
         Ok(())
